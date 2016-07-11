4c063cf68380247d51335e65b6834b83207d53e1
Guikingone <guillaume.loulier@hotmail.fr>
2016-07-11 11:30:03 +0200

    [+][!C] || Adapter Corrections|In progress

--- app/src/main/java/com/oc/actuality/oc_actuality/NewsAdapter.java
+++ app/src/main/java/com/oc/actuality/oc_actuality/NewsAdapter.java
@@ -16,13 +16,34 @@ import java.util.List;
  */
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
 
-    private List<FakeNews> list = FakeNewsList.all;
+    private List<FakeNews> _list_News = FakeNewsList.all;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView _list;
+        public ViewHolder(View view) {
+            super(view);
+            _list = (TextView) view.findViewById(R.id.title);
+        }
+        public void bind(FakeNews _list_fake) {
+            _list_fake.toString();
+        }
+    }
 
     @Override
     public int getItemCount() {
-        return list.size();
+        return _list_News.size();
     }
 
-    
+    @Override
+    public NewsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                                  .inflate(R.layout.activity_main, parent, false);
+        ViewHolder vh = new ViewHolder(view);
+        return vh;
+    }
 
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.bind(_list_News.get(position));
+    }
 }
